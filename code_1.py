c63f55e2d5d2499185c29f336ad0237d4afc732c
henrypet016 <h.pettitt@elifesciences.org>
2023-09-14 14:58:01 +0100

    updating python code

--- code_1.py
+++ code_1.py
@@ -2,14 +2,14 @@ from pyodide.http import pyfetch
 import asyncio
         
 async def fetch_data_and_write():
-        response = await pyfetch(url="https://api.elifesciences.org/articles/84711", method="GET")
-        json_response = await response.json()  # Parse the JSON response
-        title = json_response.get('title', 'Title not found')
-        output = f"GET request => status: {response.status}, title: {title}"
+    response = await pyfetch(url="https://api.elifesciences.org/articles/84711", method="GET")
+    json_response = await response.json()  # Parse the JSON response
+    title = json_response.get('title', 'Title not found')
+    output = f"{title}"
         
-        pyscript.write('titleFinal',output)
+    pyscript.write('titleFinal',output)
         
         # To run the coroutine, you need to create an asyncio event loop
-        if __name__ == "__main__":
-            loop = asyncio.get_event_loop()
-            loop.run_until_complete(fetch_data_and_write())
+    if __name__ == "__main__":
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(fetch_data_and_write())
